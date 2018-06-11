5cc3848278f0313344402bf99eba18474f03f72f
Alexandra Ackerman <aaackerman.dev@gmail.com>
2018-06-11 16:32:32 -0400

    HTML should be escaped by default in HAMLC templates.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = function(source) {
   // 
   // Compilation
   // 
-  template  = hamlc.template(source, null, null, {placement: 'standalone', escapeHtml: false});
+  template  = hamlc.template(source, null, null, {placement: 'standalone', escapeHtml: true});
   result    = "module.exports =" + template.toString();
   return result;
 }
